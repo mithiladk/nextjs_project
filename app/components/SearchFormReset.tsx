21af0f3c244458b4ac13c991f958d89ca591be28
mithiladk <mithila.deekay@gmail.com>
2024-12-17 22:48:18 +0530

    learnt about using next/form and about searchParams

---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,23 @@
+"use client"
+import Link from 'next/link';
+import React from 'react'
+
+const SearchFormReset = () => {
+    const reset = () =>{
+        const form = document.querySelector('.search-form') as
+        HTMLFormElement;
+        if(form) form.reset();
+    }
+  return (
+    <div>
+
+        <button type='reset' onClick={reset}>
+<Link href="/" className='search-btn text-white'>
+X
+</Link>
+        </button>
+    </div>
+  )
+}
+
+export default SearchFormReset
